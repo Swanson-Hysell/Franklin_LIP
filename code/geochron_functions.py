5f070112aa2e660feafe1f2d6ee8d4b62dad71db
Nick Swanson-Hysell <swanson-hysell@berkeley.edu>
2022-10-28 17:11:09 -0700

    add initial code

---
+++ code/geochron_functions.py
@@ -0,0 +1,182 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_dates(zircon_means, zircon_2sigmas, include, sample_means, sample_2sigmas, colours, labels,\
+               legend=True, figsize=None, ylim=None, lineweight=None, bbox_to_anchor=None, mean_box=None):
+    """
+    Make a date distribution plot.
+    
+    inputs:
+    - zircon_means = list of numpy arrays containing mean dates (each array being a sample)
+    - zircon_2sigmas = list of numpy arrays containing 2sigmas (each array being a sample)
+    - include = list of 'yes' or 'no' array for inclusion in mean (each array being a sample)
+    - sample_means = list of sample mean dates (one per sample)
+    - sample_2sigmas = list of sample 2sigmas (one per sample)
+    - colours = list of colours (one per sample)
+    - labels = list of labels (one per sample)
+    - legend = if True, plot legend
+    - figsize = figure size (optional)
+    - ylim = ylim (optional)
+    - linewidth = zircon linewidth (optional)
+    - bbox_to_anchor = for the legend (optional)
+    - mean_box = list of booleans (optional - each item being a sample)
+    """
+    
+    # formatting constants
+    zircon_spacing = 1
+    sample_spacing = 6 - zircon_spacing
+    start_x = 3
+    yes_alpha = 0.8
+    no_alpha = 0.2
+    if lineweight==None:
+        lineweight=3
+    
+    # initialize current x
+    current_x = start_x
+    
+    # number of samples
+    n_samples = len(zircon_means)
+    
+    # plot
+    if figsize == None:
+        fig, ax = plt.subplots()
+    else:
+        fig, ax = plt.subplots(figsize=figsize)
+    
+    # iterate over each sample
+    plot_mean_box = False
+    for i in range(n_samples):
+        
+        # number of zircons in sample
+        n_zircons = len(zircon_means[i])
+        
+        # check if we need to plot the mean box
+        if mean_box==None:
+            plot_mean_box = True
+        elif mean_box[i]:
+            plot_mean_box = True
+        
+        # iterate over each zircon to find the mean box coordinates
+        if plot_mean_box:
+            start_box = False
+            end_box = False
+            for j in range(n_zircons):
+                if include[i][j] == 'yes' and start_box == False:
+                    start_box_x = current_x + j*zircon_spacing - zircon_spacing/2
+                    start_box = True
+                if include[i][j] == 'no' and start_box == True and end_box == False:
+                    end_box_x = current_x + (j-1)*zircon_spacing + zircon_spacing/2
+                    end_box = True
+            if end_box == False:
+                end_box_x = current_x + (n_zircons-1)*zircon_spacing + zircon_spacing/2
+
+            # plot the mean box
+            # the 2sigma uncertainty on the sample age
+            if sample_2sigmas != None:
+                rectangle = matplotlib.patches.Rectangle((start_box_x, sample_means[i]-sample_2sigmas[i]),\
+                                                         (end_box_x-start_box_x), sample_2sigmas[i]*2, color='C7')
+                ax.add_patch(rectangle)
+
+            # the mean age
+            if sample_means != None:
+                ax.hlines(sample_means[i], start_box_x, end_box_x, colors='black')
+        
+        # iterate over each zircon to plot the zircons
+        marked = False # legend control
+        for j in range(n_zircons):
+            
+            if marked:
+                if include[i][j] == 'yes':
+                    ax.vlines(current_x, zircon_means[i][j]-zircon_2sigmas[i][j], zircon_means[i][j]+zircon_2sigmas[i][j],\
+                              colors=colours[i], lw=lineweight, alpha=yes_alpha)
+                # else:
+                #     ax.vlines(current_x, zircon_means[i][j]-zircon_2sigmas[i][j], zircon_means[i][j]+zircon_2sigmas[i][j],\
+                #               colors=colours[i], lw=lineweight, alpha=no_alpha)
+            else:
+                if include[i][j] == 'yes':
+                    ax.vlines(current_x, zircon_means[i][j]-zircon_2sigmas[i][j], zircon_means[i][j]+zircon_2sigmas[i][j],\
+                              colors=colours[i], lw=lineweight, alpha=yes_alpha, label=labels[i])
+                    marked = True
+                # else:
+                #     ax.vlines(current_x, zircon_means[i][j]-zircon_2sigmas[i][j], zircon_means[i][j]+zircon_2sigmas[i][j],\
+                #               colors=colours[i], lw=lineweight, alpha=no_alpha)
+                
+            # tick the x to the next zircon
+            current_x = current_x + zircon_spacing
+        
+        # tick the x to the next sample
+        current_x = current_x + sample_spacing
+        
+    # prettify
+    ax.invert_yaxis()
+    ax.set_ylabel('$^{206}$Pb/$^{238}$U date [Ma]',size=18)
+    if legend:
+        if bbox_to_anchor == None:
+            ax.legend(bbox_to_anchor=(1.8,1), fontsize=12)
+        else:
+            ax.legend(bbox_to_anchor=bbox_to_anchor, fontsize=12)
+    if ylim != None:
+        ax.set_ylim(ylim)
+        
+    return fig, ax
+
+
+def weighted_mean(dates,sigma):
+    
+    """
+    calculated a weighted mean from dates and uncertainities
+    
+    input parameters
+    ----------------
+    dates: date of individual grains
+    sigma: 1σ uncertainty associated with dates
+    
+    returns
+    ----------------
+    weighted_mean, 2σ uncertainty
+    
+    """
+    
+    inverse_variance=[]
+    alpha=[]
+    weighted_mean_calc=[]
+    
+    for n in range(0,len(dates)):
+        value = 1/sigma[n]**2
+        inverse_variance.append(value)
+    inverse_variance_sum = np.sum(inverse_variance)
+    
+    for n in range(0,len(dates)):
+        value = (1/sigma[n]**2)/inverse_variance_sum
+        alpha.append(value)
+        
+    #calculate the weights for each date    
+    for n in range(0,len(dates)):
+        value = alpha[n]*dates[n]
+        weighted_mean_calc.append(value)
+    
+    #take the sum of the weights multiplied by the dates to get the weighted
+    #mean (equation 64 of McLean et al., 2011)
+    weighted_mean = np.sum(weighted_mean_calc)
+    
+    #the variance of the weighted mean is simply the inverse of the sum of
+    #the inverse variances of each date (equation 66 of McLean et al., 2011)
+    variance = 1/inverse_variance_sum
+    
+    weighted_mean_sigma = np.sqrt(variance)
+    
+    n = len(dates)
+    
+    print('The weighted mean is:')
+
+    print(weighted_mean)
+
+    print('With a 2sigma error of:')
+
+    print(2*weighted_mean_sigma)
+    
+    print('N=' )
+    print(n)
+    
+    return weighted_mean, 2*weighted_mean_sigma
